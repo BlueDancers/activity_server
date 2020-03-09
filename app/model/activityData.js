ee4bada185a1cb61173dbabf56fa2452b8ccb543
vkcyan <wu.vkcyan@gmail.com>
2020-03-09 22:23:57 +0800

    增加动画字段

--- app/model/activityData.js
+++ app/model/activityData.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-22 12:50:34
+ * @LastEditTime: 2020-03-09 22:19:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /activity_server/app/model/activityData.js
+ */
 'use strict'
 /**
  * @param {Egg.model} app - 项目具体布局表
@@ -12,8 +20,9 @@ module.exports = app => {
     objectName: { type: String }, // 项目名称
     name: { type: String }, // dom元素映射关系
     text: { type: String }, // dom元素具体文字
-    css: { type: Object }, // 数据
-    option: { type: Object }
+    css: { type: Object }, // 样式集合
+    option: { type: Object }, // 数据集合
+    animation: {type: Object}, // 动画集合
   })
 
   return mongoose.model('activityData', DataSchema)
